c22cc861006ce465e88f36945d013b067b68291d
Balázs Dukai <balazs.dukai@gmail.com>
2018-06-07 13:44:29 +0200

    update_json_id

--- bag3d/update/ahn.py
+++ bag3d/update/ahn.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8 -*-
 
 """Update the AHN files and tile index"""
+
+
+def update_json_id(json):
+    """Update a GeoJSON tile index's ID field
+    
+    Overwrites or adds the "id" field as sequential integer
+    
+    Parameters
+    ----------
+    json: a parsed GeoJSON
+    
+    Returns
+    -------
+    The modified GeoJSON
+    """
+    for i,f in enumerate(json["features"]):
+        json["features"][i]["id"] = i+1
+        json["features"][i]["properties"]["id"] = i+1
+    return json
