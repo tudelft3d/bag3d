fdbbf0611f21c2660e1106a7b727a3813381ee62
Balázs Dukai <balazs.dukai@gmail.com>
2018-10-22 23:59:01 +0200

    Add Dockerfile for 3dfier and example data
    Based on 3dfier with lastools

---
+++ example_data/docker_test.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+# Test if the docker images work as expected
+
+# First need to run ./postgis/docker_run_postgis.sh
+
+docker run --rm --link 3dbag_postgis -v $(pwd):/home/3dfier 3dbag:3dfier 3dfier cfg_test.yml --OBJ test.obj --CSV-BUILDINGS test.csv;
+
+# Then ./postgis/docker_stop_postgis.sh
